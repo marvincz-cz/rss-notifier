f0a2fcf4a91bb6eeb5b4871d546a3bce52316492
Petr Fuka <petr.fuka@gmail.com>
2018-10-06 13:40:55 +0200

    only reflective converters called

--- app/src/main/java/marvincz/cz/rssnotifier/retrofit/Client.java
+++ app/src/main/java/marvincz/cz/rssnotifier/retrofit/Client.java
@@ -6,26 +6,11 @@ import marvincz.cz.rssnotifier.model.RssItem;
 import marvincz.cz.rssnotifier.xml.ABPZonedDateTimeConverter;
 import marvincz.cz.rssnotifier.xml.ReflectiveXmlConverter;
 import marvincz.cz.rssnotifier.xml.XmlConverterFactory;
-import marvincz.cz.rssnotifier.xml.XmlRssChannelConverter;
-import marvincz.cz.rssnotifier.xml.XmlRssConverter;
-import marvincz.cz.rssnotifier.xml.XmlRssItemConverter;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 
 public class Client {
     public static RestApi call() {
-        return new Retrofit.Builder()
-                .client(new OkHttpClient())
-                .baseUrl("http://127.0.0.1")
-                .addConverterFactory(new XmlConverterFactory.Builder()
-                        .addConverter(new XmlRssConverter())
-                        .addConverter(new XmlRssItemConverter())
-                        .addConverter(new XmlRssChannelConverter())
-                        .build())
-                .build().create(RestApi.class);
-    }
-
-    public static RestApi callReflect() {
         return new Retrofit.Builder()
                 .client(new OkHttpClient())
                 .baseUrl("http://127.0.0.1")
