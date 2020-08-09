cf27670beb75ec2be7219e541e514041d296f814
Petr Fuka <petr.fuka@gmail.com>
2020-08-09 14:35:31 +0200

    added "mark read" action to notification

--- app/src/main/java/cz/marvincz/rssnotifier/background/Worker.kt
+++ app/src/main/java/cz/marvincz/rssnotifier/background/Worker.kt
@@ -13,6 +13,7 @@ import cz.marvincz.rssnotifier.BuildConfig
 import cz.marvincz.rssnotifier.R
 import cz.marvincz.rssnotifier.RssApplication
 import cz.marvincz.rssnotifier.fragment.ChannelsFragmentArgs
+import cz.marvincz.rssnotifier.model.RssItem
 import cz.marvincz.rssnotifier.repository.Repository
 import org.koin.core.KoinComponent
 import org.koin.core.inject
@@ -27,33 +28,39 @@ class Worker(appContext: Context, params: WorkerParameters) : CoroutineWorker(ap
                     val notificationId = item.id.hashCode()
 
                     // TODO: Preference - Open browser or open the app on the channel tab
-                    val pendingIntent = if (item.link != null) {
-                        val intent = Intent(applicationContext, RedirectingService::class.java)
-                                .setData(Uri.parse(item.link))
-                                .putExtra(RedirectingService.ITEM_ID, item.id)
-                        PendingIntent.getService(applicationContext, 0, intent, 0)
-                    } else {
-                        NavDeepLinkBuilder(applicationContext)
-                                .setGraph(R.navigation.nav_graph)
-                                .setDestination(R.id.channelsFragment)
-                                .setArguments(ChannelsFragmentArgs(channel.accessUrl).toBundle())
-                                .createPendingIntent()
-                    }
+                    val pendingIntent = if (item.link != null) createPendingIntent(item)
+                    else NavDeepLinkBuilder(applicationContext)
+                            .setGraph(R.navigation.nav_graph)
+                            .setDestination(R.id.channelsFragment)
+                            .setArguments(ChannelsFragmentArgs(channel.accessUrl).toBundle())
+                            .createPendingIntent()
 
-                    val builder = NotificationCompat.Builder(applicationContext, RssApplication.NOTIFICATION_CHANNEL_ID)
+                    val markReadPendingIntent = createPendingIntent(item, false)
+                    val markReadAction = NotificationCompat.Action(0, applicationContext.getString(R.string.action_mark_read), markReadPendingIntent)
+
+                    val notificationBuilder = NotificationCompat.Builder(applicationContext, RssApplication.NOTIFICATION_CHANNEL_ID)
                             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                             .setSmallIcon(R.drawable.ic_rss_feed)
                             .setContentTitle(channel.title)
                             .setContentText(item.title)
                             .setContentIntent(pendingIntent)
+                            .addAction(markReadAction)
                             .setAutoCancel(true)
 
                     with(NotificationManagerCompat.from(applicationContext)) {
                         // notificationId is a unique int for each notification that you must define
-                        notify(notificationId, builder.build())
+                        notify(notificationId, notificationBuilder.build())
                     }
                 }
 
         return Result.success()
+    }
+
+    private fun createPendingIntent(item: RssItem, redirect: Boolean = true): PendingIntent {
+        val intent = Intent(applicationContext, RedirectingService::class.java)
+                .putExtra(RedirectingService.ITEM_ID, item.id)
+        if (redirect) intent.data = Uri.parse(item.link)
+
+        return PendingIntent.getService(applicationContext, 0, intent, 0)
     }
 }
