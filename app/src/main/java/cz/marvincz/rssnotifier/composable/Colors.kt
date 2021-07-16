10f8cc32ace2f6120e02c5f3056fbc37553e7b84
Petr Fuka <petr.fuka@gmail.com>
2021-07-16 20:35:23 +0200

    dark secondary color

--- app/src/main/java/cz/marvincz/rssnotifier/composable/Colors.kt
+++ app/src/main/java/cz/marvincz/rssnotifier/composable/Colors.kt
@@ -9,24 +9,25 @@ private val Teal500 = Color(0xFF009688)
 private val Teal700 = Color(0xFF00796B)
 
 private val Purple200 = Color(0xFFCE93D8)
+private val Purple200Dark = Color(0xFF9C64A6)
 
 private val Black54 = Color.Black.copy(alpha = 0.54f)
 private val White54 = Color.White.copy(alpha = 0.54f)
 
 private fun primary(dark: Boolean) = if (dark) Teal200 else Teal500
 private fun primaryVariant() = Teal700
-private fun secondary() = Purple200
+private fun secondary(dark: Boolean) = if (dark) Purple200Dark else Purple200
 
 fun iconOnSurface(dark: Boolean) = if (dark) White54 else Black54
 
 fun colors(dark: Boolean) = if (dark) darkColors(
     primary = primary(dark),
     primaryVariant = primaryVariant(),
-    secondary = secondary(),
-    secondaryVariant = secondary()
+    secondary = secondary(dark),
+    secondaryVariant = secondary(dark)
 ) else lightColors(
     primary = primary(dark),
     primaryVariant = primaryVariant(),
-    secondary = secondary(),
-    secondaryVariant = secondary()
+    secondary = secondary(dark),
+    secondaryVariant = secondary(dark)
 )
